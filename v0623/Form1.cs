f9de4e747f0380d64c87296ad65294e42f5083f4
A601-11 <tnk.dat.class@gmail.com>
2022-06-23 13:28:40 +0900

    v0623

--- v0623/Form1.cs
+++ v0623/Form1.cs
@@ -12,10 +12,44 @@ namespace v0623
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
+
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("杉田夏葵");
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = 10;
+            }
+            if (label1.Right > ClientSize.Width)
+            {
+                vx = -10;
+            }
+            if (label1.Top < 0)
+            {
+                vy = 10;
+            }
+            if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -10;
+            }
+        }
     }
 }
