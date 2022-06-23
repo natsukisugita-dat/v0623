d10d76ea2878e2bcc2fce8146117acbd8a0a9970
A601-11 <tnk.dat.class@gmail.com>
2022-06-23 15:47:51 +0900

    演習10

--- v0623/Form1.Designer.cs
+++ v0623/Form1.Designer.cs
@@ -33,6 +33,7 @@ namespace v0623
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -60,11 +61,22 @@ namespace v0623
             this.label2.Text = "Score 0";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(291, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -79,6 +91,7 @@ namespace v0623
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- v0623/Form1.cs
+++ v0623/Form1.cs
@@ -30,14 +30,21 @@ namespace v0623
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            Point spos = MousePosition;
+            Point fpos = PointToClient(spos);
+            label3.Left = fpos.X-label3.Width/2;
+            //label3.Left = fpos.X + (label3.Left - label3.Right) / 2; これでもできる(やってることは同じ)
+            label3.Top = fpos.Y-label3.Height/2;
+            label3.Text = $"{fpos.X},{fpos.Y}";//マウスの座標表示
+
             timer1.Enabled = true;
 
             label1.Left += vx;
             label1.Top += vy;
 
             score -= 1;
-            //label2.Text = $"score {score}";
-            label2.Text = ("score "+ score); //これでもできる
+            label2.Text = $"score {score}";
+            //label2.Text = ("score "+ score); //これでもできる
 
             if (label1.Left < 0)
             {
